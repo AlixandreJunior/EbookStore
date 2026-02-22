a1b5a9aff25eadf3a71983e8b9b8cccba8844e0d
Alixandria007 <juninhokrauts778@gmail.com>
2026-02-22 00:11:23 -0300

    Backend criado!

--- backend/src/apps/affiliate/apps.py
+++ backend/src/apps/affiliate/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UserConfig(AppConfig):
+class AffiliateConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user'
+    name = 'affiliate'
